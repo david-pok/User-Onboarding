0faabb2d2a09ef458069fed41c178fc5810a7450
david-pok <david.pok@gmail.com>
2020-01-30 22:04:20 -0800

    mvp done

---
+++ advanced-forms/src/dummyData.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    id: 0,
+    name: "Bruce",
+    email: "imbatman@dc.com",
+    password: "imbatman",
+    team: "DC",
+    info: "broody detective"
+  },
+  {
+    id: 1,
+    name: "Clark",
+    email: "boyscout@dc.com",
+    password: "imsuperman",
+    team: "DC",
+    info: "the hero"
+  },
+  {
+    id: 2,
+    name: "Tony",
+    email: "iamironman@marvel.com",
+    password: "iamironman",
+    team: "Marvel",
+    info: "billionaire playboy philathropist",
+  },
+  {
+    id: 3,
+    name: "Steve",
+    email: "",
+    password: "avengers assemble",
+    team: "Marvel",
+    info: "the ultimate soldier"
+  }
+];
